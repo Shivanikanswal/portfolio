35ca671d29dbb7d212e2e48b3050a734b35e6e09
Shivani Kanswal <shivanikanswal@gmail.com>
2024-03-12 23:24:05 +0530

    Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,17 @@
 const Footer = () => {
-  return <div id="footer">Hi I am Footer Component</div>;
+  return (
+    <footer className=" flex justify-between px-14 py-20">
+      <div className="designedby text-base font-semibold font-sans">
+        Designed and Developed by Shivani Kanswal 2024
+      </div>
+      <div className="footer-links">
+        <span></span>
+        <a href="">LinkedIn</a>
+        <a href="">Email</a>
+        <a href="">Github</a>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
