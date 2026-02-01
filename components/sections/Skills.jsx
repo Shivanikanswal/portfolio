115e50dc97558c20d843cfac01d4dca325dc8ae4
Shivani Kanswal <shivanikanswal@gmail.com>
2026-02-01 22:35:24 +0530

    skills section added

--- components/sections/Skills.jsx
+++ components/sections/Skills.jsx
@@ -1,12 +1,39 @@
 import Section from "../layout/SectionWrapper";
+import { skills } from "../../public/data/skill-data";
+import Image from "next/image";
+
 const Skills = () => {
   return (
-    <Section id="skills" className="pt-10 sm:pt-24">
-      <div>
-        <h2 className="text-4xl font-semibold"> Tools and Technologies </h2>
-        <div></div>
+    <section
+      id="skills"
+      className="flex justify-center py-10 sm:py-24 px-6 w-full bg-linear-to-b from-[#f7f7f7] to-[#efefef] "
+    >
+      <div className="flex flex-col gap-12 max-w-6xl mx-auto items-center">
+        <p className=" text-2xl sm:text-5xl font-semibold text-neutral-500">
+          {" "}
+          Tools and Technologies{" "}
+        </p>
+        <div className="skill-card grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-x-48 gap-y-8 place-items-center">
+          {skills.map((item, index) => {
+            return (
+              <div key={index} className="flex flex-col gap-3 p-2 items-center">
+                <div className="w-16 h-16 rounded-lg bg-white p-2 shadow-sm">
+                  <Image
+                    src={item.logo}
+                    alt="skills-logo"
+                    width={56}
+                    height={56}
+                  />
+                </div>
+                <p className=" text-xs sm:text-sm font-bold text-gray-700 tracking-wider">
+                  {item.name}
+                </p>
+              </div>
+            );
+          })}
+        </div>
       </div>
-    </Section>
+    </section>
   );
 };
 export default Skills;
---
+++ public/data/skill-data.jsx
@@ -0,0 +1,54 @@
+export const skills = [
+  {
+    name: "HTML5",
+    logo: "utils/skills/HTML5.svg",
+  },
+  {
+    name: "CSS3",
+    logo: "utils/skills/CSS3.svg",
+  },
+  {
+    name: "JavaScript",
+    logo: "utils/skills/JavaScript.svg",
+  },
+  {
+    name: "React",
+    logo: "utils/skills/React.svg",
+  },
+  {
+    name: "Redux",
+    logo: "utils/skills/Redux.svg",
+  },
+  {
+    name: "Tailwind",
+    logo: "utils/skills/Tailwind CSS.svg",
+  },
+  {
+    name: "Jest",
+    logo: "utils/skills/Jest.svg",
+  },
+  {
+    name: "Babel",
+    logo: "utils/skills/Babel.svg",
+  },
+  {
+    name: "Typescript",
+    logo: "utils/skills/TypeScript.svg",
+  },
+  {
+    name: "ESLint",
+    logo: "utils/skills/ESLint.svg",
+  },
+  {
+    name: "Next",
+    logo: "utils/skills/next.svg",
+  },
+  {
+    name: "Git",
+    logo: "utils/skills/Git.svg",
+  },
+  {
+    name: "Vite",
+    logo: "utils/skills/Vite.svg",
+  },
+];
